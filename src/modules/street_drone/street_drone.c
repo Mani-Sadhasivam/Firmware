7eda010f28759799929a3b202d4b742ea0f790bc
Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>
2018-07-13 15:30:13 +0530

    street_drone: Modify module to send CAN data received from Master
    Signed-off-by: Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>

--- src/modules/street_drone/street_drone.c
+++ src/modules/street_drone/street_drone.c
@@ -167,20 +167,26 @@ __EXPORT int street_drone_main(int argc, char *argv[]);
 int street_drone_main(int argc, char *argv[])
 {
 	ssize_t nbytes;
-	int ret, nread, msglen, i, found = 0;
 	ssize_t msgsize, minsize;
-	int8_t str_req, thr_req;
-
+	uint8_t rx_data[CAN_MAXDATALEN];
+	uint8_t str_req, thr_req, str_auto, thr_auto;
+	uint8_t str_max, str_min, thr_max, thr_min;
+	int ret, nread, msglen, i, found = 0;
+	
 	int send_topic = orb_subscribe(ORB_ID(debug_vect));
         px4_pollfd_struct_t fds[1] = {};
         fds[0].fd = send_topic;
         fds[0].events = POLLIN;
 
+//	struct named_value_int_s int_value;
+//	memset(&int_value, 0, sizeof(int_value));
+//	orb_advert_t int_value_fd = orb_advertise(ORB_ID(named_value_int), &int_value);
+
 	fflush(stdout);
 
 	if (argc < 2) {
 		exit(1);
-	} else {
+	} else if (!strcmp(argv[1], "send")) {
 		PX4_INFO("Initializing the CAN device");
 		/* Initialize the CAN device */
 		ret = can_devinit();
@@ -205,77 +211,81 @@ int street_drone_main(int argc, char *argv[])
 			if (fds[0].revents & POLLIN) {
 				struct debug_vect_s data;
 				orb_copy(ORB_ID(debug_vect), send_topic, &data);
-				PX4_INFO("Sending data:\nSteer Req: %d\nThrottle Req: %d", data.x, data.y);
+//				memcpy(int_value.name, name, 10);
+//				PX4_INFO("Pusblishing data\n");
+//				int_value.value = data.x;
+//				orb_publish(ORB_ID(named_value_int), int_value_fd, &int_value);
+
+				PX4_INFO("Constructing the CAN frame for transmission");
+				msgsize = CAN_MSGLEN(MSG_DLC);
+
+				str_req = data.str_req;
+				thr_req = data.thr_req;
+				str_auto = data.str_auto;
+				thr_auto = data.thr_auto;
+				cmd_cus_ctrl_1(&txmsg, str_req, thr_req, str_auto, thr_auto);
+
+				PX4_INFO("Sending the CAN frame");
+				/* Send CAN frame */
+				nbytes = write(fd, &txmsg, msgsize);
+				if (nbytes != msgsize) {
+					PX4_ERR("Unable to write to CAN device");
+					close(fd);
+					return -1;
+				}
+
+				PX4_INFO("Sent bytes: %u successfully", nbytes);
 			}
 		}
+	} else if (!strcmp(argv[1], "recv")) {
+		PX4_INFO("Reading the CAN frames");
+		/* Read CAN frames */
+		minsize = CAN_MSGLEN(0);
+		msgsize = sizeof(struct can_msg_s);
+		nread = read(fd, rxbuffer, BUFLEN);
+		if (nread < minsize || nread > msgsize) {
+			PX4_ERR("Unable to read from CAN device");
+			close(fd);
+			return -1;
+		}
+		
+		PX4_INFO("Received bytes: %u successfully", nread);
+		for (i = 0; i <= nread - minsize; i += msglen) {	
+  			/* Get the next message from the RX buffer */
+			rxmsg  = (struct can_msg_s *)&rxbuffer[i];
+			msglen = CAN_MSGLEN(8);
 
-		if (!strcmp(argv[1], "send")) {
-			PX4_INFO("Constructing the CAN frame for transmission");
-			msgsize = CAN_MSGLEN(MSG_DLC);
+			DEBUGASSERT(i + msglen < BUFLEN);
 
 			/* 
-			 * FIXME: These parameters should come from the Autoware stack through UART
-			 */
-			str_req = -50;
-			thr_req = -20;
-			str_req = (str_req < 0 ? (128 - str_req) : str_req);
-			thr_req = (thr_req < 0 ? (128 - thr_req) : thr_req);
-			cmd_cus_ctrl_1(&txmsg, str_req, thr_req, 1, 1);
-
-			PX4_INFO("Sending the CAN frame");
-			/* Send CAN frame */
-			nbytes = write(fd, &txmsg, msgsize);
-			if (nbytes != msgsize) {
-				PX4_ERR("Unable to write to CAN device");
-				close(fd);
-				return -1;
-			}
-
-			PX4_INFO("Sent bytes: %u successfully", nbytes);
-//		} else if (!strcmp(argv[1], "recv")) {
-			PX4_INFO("Reading the CAN frames");
-			/* Read CAN frames */
-			minsize = CAN_MSGLEN(0);
-			msgsize = sizeof(struct can_msg_s);
-			nread = read(fd, rxbuffer, BUFLEN);
-			if (nread < minsize || nread > msgsize) {
-				PX4_ERR("Unable to read from CAN device");
-				close(fd);
-				return -1;
-			}
-			
-			PX4_INFO("Received bytes: %u successfully", nread);
-			for (i = 0; i <= nread - minsize; i += msglen) {	
-  				/* Get the next message from the RX buffer */
-				rxmsg  = (struct can_msg_s *)&rxbuffer[i];
-				msglen = CAN_MSGLEN(8);
-
-				DEBUGASSERT(i + msglen < BUFLEN);
-
-				/* 
-				 * FIXME: Ideally we should compare the received header with
-				 * StreetDrone_Control_1 header stored in ctrlmsg
-			 	 */
-				if (!memcmp(&rxmsg->cm_hdr, &txmsg.cm_hdr, sizeof(struct can_hdr_s))) {
-					found = 1;
-					break;
-				}
-			}
-			printf("Recv ID: %4u DLC: %u\n", rxmsg->cm_hdr.ch_id, rxmsg->cm_hdr.ch_dlc);
-			if (found) {
-				PX4_INFO("StreetDrone_Control_1 CAN frame received!");
-				PX4_INFO("Steer Request Max: %u", rxmsg->cm_data[2]);	
-				PX4_INFO("Steer Request Min: %u", rxmsg->cm_data[3]);	
-				PX4_INFO("Torque Request Min: %u", rxmsg->cm_data[4]);	
-				PX4_INFO("Torque Request Min: %u", rxmsg->cm_data[5]);
-				PX4_INFO("Steer Automation State: %u", ((rxmsg->cm_data[6] >> 4) & 0x0F));
-				PX4_INFO("Torque Automation State: %u", (rxmsg->cm_data[6] & 0x0F));
-			} else {
-				PX4_WARN("StreetDrone_Control_1 CAN frame not recieved");
+			 * FIXME: Ideally we should compare the received header with
+			 * StreetDrone_Control_1 header stored in ctrlmsg
+		 	 */
+			if (!memcmp(&rxmsg->cm_hdr, &txmsg.cm_hdr, sizeof(struct can_hdr_s))) {
+				found = 1;
+				break;
 			}
 		}
-
-		close(fd);
-		exit(0);
+		if (found) {
+			printf("Recv ID: %4u DLC: %u\n", rxmsg->cm_hdr.ch_id, rxmsg->cm_hdr.ch_dlc);
+			memcpy(&rx_data, &rxmsg->cm_data, sizeof(rxmsg->cm_data));
+			str_max = (rx_data[2] > 127 ? (rx_data[2] - 256) : rx_data[2]);
+			str_min = (rx_data[3] > 127 ? (rx_data[3] - 256) : rx_data[3]);
+			thr_max = (rx_data[4] > 127 ? (rx_data[4] - 256) : rx_data[4]);
+			thr_min = (rx_data[5] > 127 ? (rx_data[5] - 256) : rx_data[5]);
+			
+			PX4_INFO("StreetDrone_Control_1 CAN frame received!");
+			PX4_INFO("Steer Request Max: %u", str_max);	
+			PX4_INFO("Steer Request Min: %u", str_min);	
+			PX4_INFO("Torque Request Min: %u", thr_max);	
+			PX4_INFO("Torque Request Min: %u", thr_min);
+			PX4_INFO("Steer Automation State: %u", ((rx_data[6] >> 4) & 0x0F));
+			PX4_INFO("Torque Automation State: %u", (rx_data[6] & 0x0F));
+		} else {
+			PX4_WARN("StreetDrone_Control_1 CAN frame not recieved");
+		}
 	}
+
+	close(fd);
+	exit(0);
 }
